45cfa24c0c0437f0d1c4d3335b8e8c008d1e3514
Mark Brooks <markabrooks@gmail.com>
2024-03-19 13:53:29 -0600

    16 use amplify UI for styling and hook up react router (#17)

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import { useEffect, useState } from 'react';
-
-import { createZone } from './graphql/mutations';
 import { listZones } from './graphql/queries';
 import { type CreateZoneInput, type Zone } from './API';
-import './App.css';
 
 import { generateClient } from 'aws-amplify/api';
 import { Amplify } from 'aws-amplify';
-import { Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator, Button, Grid, Heading, Flex} from '@aws-amplify/ui-react';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import ZoneDetail from './components/ZoneDetail';
 
 import '@aws-amplify/ui-react/styles.css';
 
+import './App.css';
 import ZoneCard from './components/ZoneCard';
-
-import config from './amplifyconfiguration.json';
 import authComponents from './components/authComponents';
 
+import config from './amplifyconfiguration.json';
 Amplify.configure(config);
 
-const initialState: CreateZoneInput = { id: '', title: '' };
 const client = generateClient();
 
 export default function App() {
-  const [formState, setFormState] = useState<CreateZoneInput>(initialState);
   const [zones, setZones] = useState<Zone[] | CreateZoneInput[]>([]);
 
   useEffect(() => {
@@ -41,88 +40,27 @@ export default function App() {
     }
   }
 
-  async function addZone() {
-    try {
-      if (!formState.id || !formState.title) return;
-      const zone = { ...formState };
-      setZones([...zones, zone]);
-      setFormState(initialState);
-      const data = await client.graphql({
-        query: createZone,
-        variables: {
-          input: zone,
-        },
-      });
-      console.log("data", data)
-    } catch (err) {
-      console.log('error creating zone:', err);
-    }
-  }
   return (
     <Authenticator components={authComponents}>
       {({ signOut }) => (
-        <div style={styles.container}>
-        <header style={styles.header}>Zones</header>
-          {zones.map((zone, index) => (
-            <ZoneCard key={zone.id ? zone.id : index} zone={zone}/>
-          ))}
-          <h2>Create a zone</h2>
-          <input
-            onChange={(event) =>
-              setFormState({ ...formState, title: event.target.value })
-            }
-            style={styles.input}
-            value={formState.title as string}
-            placeholder="title"
-          />
-          <input
-            onChange={(event) =>
-              setFormState({ ...formState, id: event.target.value })
-            }
-            style={styles.input}
-            value={formState.id || ''}
-            placeholder="id"
-          />
-          <button style={styles.button} onClick={addZone}>
-            Create Zone
-          </button>
-          <button style={styles.signOut} onClick={signOut}>Sign out</button>
-        </div>
+        <BrowserRouter>
+          <Routes>
+          <Route path='/' element={
+              <Flex direction="column" alignItems="center" justifyContent="center">
+                <Heading color="white" textAlign="center">Zones</Heading>
+                <Grid templateColumns="repeat(auto-fit, minmax(300px, 1fr))" gap="20px" width="100%" justifyContent="center">
+                  {zones.map((zone, index) => (
+                    <ZoneCard key={zone.id ? zone.id : index} zone={zone} />
+                  ))}
+                </Grid>
+                <Button backgroundColor="grey" color="white"
+                  onClick={signOut} width="300px" variation="link" marginTop="20px">Sign out</Button>
+              </Flex>
+          }/>
+          <Route path='/zone/:zoneId' element={<ZoneDetail />} />
+        </Routes>
+        </BrowserRouter>
       )}
     </Authenticator>
   );
 };
-
-const styles = {
-  container: {
-    width: 400,
-    margin: "0 auto",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    padding: 20,
-  },
-  input: {
-    border: "none",
-    backgroundColor: "black",
-    marginBottom: 10,
-    padding: 8,
-    fontSize: 18,
-  },
-  zonetitle: { fontSize: 20, fontWeight: "bold" },
-  zoneid: { marginBottom: 0 },
-  button: {
-    backgroundColor: "#3c5c47",
-    color: "white",
-    outline: "none",
-    fontSize: 18,
-    padding: "12px 0px",
-  },
-  signOut: {
-    marginTop: '30px',
-  },
-  header: {
-    paddingTop: '30px',
-    fontSize: 30,
-  },
-} as const;
--- src/components/ZoneCard.tsx
+++ src/components/ZoneCard.tsx
@@ -1,44 +1,32 @@
+import React from 'react';
+import { View, Text, Card } from '@aws-amplify/ui-react';
 import { type CreateZoneInput, type Zone } from '../API'
+import { Link } from 'react-router-dom'
 
 interface ZoneCardProps {
     zone: CreateZoneInput | Zone;
 }
 
 const ZoneCard: React.FC<ZoneCardProps> = ({ zone }) => {
-
     const isZone = (object: any): object is Zone => {
         return 'id' in object && 'title' in object;
     };
 
     if (!isZone(zone)) {
-        // Handle the case where zone is not a Zone type
-        return null; // Or some other fallback UI
+        return null;
     }
 
-    const cardStyles = {
-        border: '1px solid #ccc',
-        borderRadius: '8px',
-        padding: '10px',
-        margin: '10px',
-        textAlign: 'center' as const,
-        backgroundColor: '#3c5c47',
-    };
-
-    const titleStyles = {
-        fontSize: '1.2em',
-        fontWeight: 'bold',
-        marginBottom: '5px',
-    };
-
-    const idStyles = {
-        color: '#555',
-    };
-
     return (
-        <div style={cardStyles}>
-        <p style={titleStyles}>{zone.title}</p>
-        <p style={idStyles}>{zone.id}</p>
-        </div>
+        <Link to={`/zone/${zone.id}`} style={{ textDecoration: 'none' }}>
+            <Card variation="elevated" style={{ margin: '10px', width: "300px", backgroundColor: '#3c5c47', borderRadius: '10px' }}>
+                <View style={{ textAlign: 'center',  backgroundColor: '#3c5c47' }}>
+                    <Text style={{ color: 'lightGrey', fontSize: '1.2em', fontWeight: 'bold', marginBottom: '5px' }}>
+                        {zone.title}
+                    </Text>
+                    <Text style={{ color: 'lightGrey' }}>{zone.id}</Text>
+                </View>
+            </Card>
+        </Link>
     );
 };
 
