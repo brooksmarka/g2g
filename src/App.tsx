197d7acf2b20404e40db7c625d9ca225dc0679d3
Mark Brooks <markabrooks@gmail.com>
2024-03-19 11:55:42 -0600

    use amplify-ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
 import { useEffect, useState } from 'react';
-
-import { createZone } from './graphql/mutations';
 import { listZones } from './graphql/queries';
 import { type CreateZoneInput, type Zone } from './API';
-import './App.css';
 
 import { generateClient } from 'aws-amplify/api';
 import { Amplify } from 'aws-amplify';
-import { Authenticator } from '@aws-amplify/ui-react';
-
+import { Authenticator, Button, Heading, Grid } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
+import './App.css';
 import ZoneCard from './components/ZoneCard';
-
-import config from './amplifyconfiguration.json';
 import authComponents from './components/authComponents';
 
+import config from './amplifyconfiguration.json';
 Amplify.configure(config);
 
-const initialState: CreateZoneInput = { id: '', title: '' };
 const client = generateClient();
 
 export default function App() {
-  const [formState, setFormState] = useState<CreateZoneInput>(initialState);
   const [zones, setZones] = useState<Zone[] | CreateZoneInput[]>([]);
 
   useEffect(() => {
@@ -41,88 +35,20 @@ export default function App() {
     }
   }
 
-  async function addZone() {
-    try {
-      if (!formState.id || !formState.title) return;
-      const zone = { ...formState };
-      setZones([...zones, zone]);
-      setFormState(initialState);
-      const data = await client.graphql({
-        query: createZone,
-        variables: {
-          input: zone,
-        },
-      });
-      console.log("data", data)
-    } catch (err) {
-      console.log('error creating zone:', err);
-    }
-  }
   return (
     <Authenticator components={authComponents}>
       {({ signOut }) => (
-        <div style={styles.container}>
-        <header style={styles.header}>Zones</header>
+        <>
+          <Heading level={3} color = "white" padding="30px 0 0 40px">Zones</Heading>
+        <Grid templateColumns="repeat(1, 1fr)" gap="5px">
           {zones.map((zone, index) => (
-            <ZoneCard key={zone.id ? zone.id : index} zone={zone}/>
+            <ZoneCard key={zone.id ? zone.id : index} zone={zone} />
           ))}
-          <h2>Create a zone</h2>
-          <input
-            onChange={(event) =>
-              setFormState({ ...formState, title: event.target.value })
-            }
-            style={styles.input}
-            value={formState.title as string}
-            placeholder="title"
-          />
-          <input
-            onChange={(event) =>
-              setFormState({ ...formState, id: event.target.value })
-            }
-            style={styles.input}
-            value={formState.id || ''}
-            placeholder="id"
-          />
-          <button style={styles.button} onClick={addZone}>
-            Create Zone
-          </button>
-          <button style={styles.signOut} onClick={signOut}>Sign out</button>
-        </div>
+          <Button backgroundColor = "grey" color ="white"
+            onClick={signOut} variation="link" marginTop="20px">Sign out</Button>
+              </Grid>
+          </> 
       )}
     </Authenticator>
   );
 };
-
-const styles = {
-  container: {
-    width: 400,
-    margin: "0 auto",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    padding: 20,
-  },
-  input: {
-    border: "none",
-    backgroundColor: "black",
-    marginBottom: 10,
-    padding: 8,
-    fontSize: 18,
-  },
-  zonetitle: { fontSize: 20, fontWeight: "bold" },
-  zoneid: { marginBottom: 0 },
-  button: {
-    backgroundColor: "#3c5c47",
-    color: "white",
-    outline: "none",
-    fontSize: 18,
-    padding: "12px 0px",
-  },
-  signOut: {
-    marginTop: '30px',
-  },
-  header: {
-    paddingTop: '30px',
-    fontSize: 30,
-  },
-} as const;
