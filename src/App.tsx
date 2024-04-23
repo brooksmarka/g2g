f402f4ea80f0823fcb9d64c7a88b5dd4c030c390
Mark Brooks <markabrooks@gmail.com>
2024-04-23 15:20:51 -0600

    21 show correct status on zonedetail page (#24)
    * show status on zone page
    
    * fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { generateClient } from 'aws-amplify/api';
 import { Amplify } from 'aws-amplify';
 import { Authenticator, Button, Grid, Heading, Flex} from '@aws-amplify/ui-react';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import ZoneDetail from './components/ZoneDetail';
 import ListTrails from './components/ListTrails';
@@ -58,6 +58,7 @@ export default function App() {
                   onClick={signOut} width="300px" variation="link" marginTop="20px">Sign out</Button>
               </Flex>
           }/>
+          <Route path='/zone/:zoneId/' element={<Navigate replace to={`/zone/:zoneId`} />} />
           <Route path='/zone/:zoneId' element={<ZoneDetail />} />
           <Route path="/zone/:zoneId/trails" element={<ListTrails />} />  
         </Routes>
